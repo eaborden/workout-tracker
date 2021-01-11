c54cd4a23c3406fded9b0c1bc23e0b90c3b41169
Edward Borden <arroll.borden@gmail.com>
2021-01-11 18:31:37 -0500

    Server.js file with routes requires and used

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes/api-routes");
+// require("./routes/html-routes");
+
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+//console.log(body)
+app.post("/submit", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
